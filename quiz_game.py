5f7d9c61209b6998ec6ad2a8bf48602d125916fa
Jay Raine <jycalv95@gmail.com>
2023-07-06 17:03:55 +0100

    Fixing the initial commit

---
+++ quiz_game.py
@@ -0,0 +1,5 @@
+questions = ["What is the largest planet in our solar system?","Who plays Indiana Jones?","What is the name of the most northern point of mainland Brittan?",
+             "What country has the highest life expectancy?", "What company was originally called Cadabra?", "What is the worlds fastest bird?",
+             "What country has won the most World Cups?"]
+# correct answers q1 B Jupiter, q2 C Harrison Ford, q3 A John O' Groats, q4 C Hong Kong, q5 A Amazon, q6 B The Peregrine Falcon q7 C Brazil
+answers = ["B","C", "A","C","A","B","C"]
