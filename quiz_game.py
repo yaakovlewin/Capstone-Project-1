ead04eb948eb1f33130510a859d3fe138792a114
Jay Raine <97954456+MooseTaesh@users.noreply.github.com>
2023-07-06 18:53:34 +0100

    Adding functions and prepping for loops

--- quiz_game.py
+++ quiz_game.py
@@ -3,4 +3,27 @@ questions = ["What is the largest planet in our solar system?","Who plays Indian
              "What country has the highest life expectancy?", "What company was originally called Cadabra?", "What is the worlds fastest bird?",
              "What country has won the most World Cups?"]
 # correct answers q1 B Jupiter, q2 C Harrison Ford, q3 A John O' Groats, q4 C Hong Kong, q5 A Amazon, q6 B The Peregrine Falcon q7 C Brazil
-answers = ["B","C", "A","C","A","B","C"]
+correctanswers = ["B","C", "A","C","A","B","C"]
+choices = ["A","B","C"]
+q1 = ("")
+q2 = ("")
+q3 = ("")
+q4 = ("")
+q5 = ("")
+q6 = ("")
+q7 = ("")
+basescore = 0
+score = 10
+def trackscore():
+    basescore + score
+
+
+print("Welcome to our amazing quiz! Im sure you will have a great time!")
+print("You will be shown a question and a choice of three answers.")
+print("The only thing you need to type in is either A B or C")
+name = str(input("What is your name? Please add it here: "))
+print("Hello ",name,"Lets get going!")
+while True:
+    print(questions[0])
+
+
